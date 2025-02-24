7cfc4c2c4650c3357a077884046039dd949da3ce
frncg1tsalvad0r <132061377+frncg1tsalvad0r@users.noreply.github.com>
2025-02-24 15:52:25 +0100

    Aggiunto Rimozione in coda e rimozione in testa nella lista lincata

--- ListaLicataPrj/src/App.java
+++ ListaLicataPrj/src/App.java
@@ -1,9 +1,30 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
+        /*
+        ll.size(); // dimensione della lista
+        ll.get(int indice); 
+        ll.remove(indice):
+        ll.addFirst(ll);
+        ll.addLast();
+        ll.toString();
+        */
         ListaLincataInt lii = new ListaLincataInt();
         lii.addFirst(15);
-        lii.add(12);
+        lii.addFirst(20);
+        lii.addFirst(30);
+        lii.addLast(999);
+
         System.out.println(lii.toString());
-        lii.remove(0);
+        System.out.println("Il numero di elementi è " + lii.size());
+
+        lii.removeFirst();
+        System.out.println("Rimosso l'elemento in testa");
+        System.out.println(lii.toString());
+
+        lii.removeLast();
+        System.out.println("Rimosso l'elemento in coda");
+        System.out.println(lii.toString());
+
     }
 }
--- ListaLicataPrj/src/ListaLincataInt.java
+++ ListaLicataPrj/src/ListaLincataInt.java
@@ -1,21 +1,171 @@
 public class ListaLincataInt {
     private Nodo testa = null;
+    private int contaNodi;
     
     public ListaLincataInt() {
+        this.testa = null;
+        this.contaNodi = 0;
+    }
+
+    /**
+     * Inserisce un elemento in testa alla lista lincata
+     * @param dato
+     */
+    public void addFirst(int dato){
+        Nodo nuovo = new Nodo();
+        nuovo.valore = dato;
+        nuovo.successivo = testa;
+        testa = nuovo;
+
+        this.contaNodi++;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        Nodo corrente = this.testa;
+
+        while(true) {
+            if(corrente == null)
+                break;
+            
+            s += corrente.valore + " ";
+            
+            corrente = corrente.successivo;
+        }
+
+        /*
+        while(corrente == null) {
+            s += corrente.valore + " ";
+            corrente = corrente.successivo;
+        }
+         */
+        return s;
+    }
+
+    /** 
+     * Ritorna il numero di elementi della lista lincata
+     * @return la dimensione della lista lincata
+     */
+    public int size() {
+        /*
+        Non uso il contatore aggiornato ma
+        conto i nodi uno ad uno
+        return this.contaNodi;
+         */
+        int conta = 0;
+        Nodo corrente = null;
+        corrente = this.testa;
+        
+        
+        while(true) {
+            if (corrente == null) {
+                break;
+            }
+            conta++;
+            corrente = corrente.successivo;
+        }   
+        return conta;
+    }
+
+    /**
+     * Aggiunge un elemento in coda alla lista
+     * @param dato
+     */
+    void addLast(int dato) {
+        Nodo nuovo = new Nodo();
+        nuovo.valore = dato;
+        nuovo.successivo = null;
+
+        if(this.testa == null){
+            this.testa = nuovo;
+        } else {
+            Nodo corrente = this.testa;
+            while(true){
+                if(corrente.successivo == null)
+                    break;
+                corrente = corrente.successivo;
+            }
+            corrente.successivo = nuovo;
+        }
+        this.contaNodi++;
+    }
+
+    /**
+     * Rimuovere l'elemento in testa alla lista
+     * @return
+     */
+    public int removeFirst() {
+        if(this.testa == null) {
+            // la lista è vuota e decido cosa fare
+            throw new RuntimeException("Lista vuota");
+
+        }
+        int tmp =this.testa.valore;
+        this.testa = this.testa.successivo;
+        this.contaNodi--;
+        return tmp;
 
     }
 
-    public void addFirst(int valore){
-        // CREO IL NODO
-        Nodo nuovo = new Nodo();
-        // ASSEGNO AL NODO NUOVO IL VALORE PASSATO COME PARAMETRO
-        nuovo.valore = valore;
-         
-        // IL SUCCESSIVO DEL NUOVO è LO STESSO DELLA TESTA
-        nuovo.successivo = testa;
-        // LA TESTA PUNTERA' AL NUOVO NODO
-        testa = nuovo;
-       
+    /**
+     * Rimuove l'elemento in coda alla lista lincata
+     * @return
+     */
+    public int removeLast() {
+        if(this.testa == null) {
+            // la lista è vuota e decido cosa fare
+            throw new RuntimeException("Lista vuota");
+
+        }
+        // Ho almeno un elemento nella lista
+
+
+        if(this.testa.successivo == null) {
+            // Sono nel caso in cui ho un sol valore
+            int tmp =  this.testa.valore;
+            this.testa = null;
+            return tmp;
+        } 
+
+        /*
+        Nodo corrente = this.testa;
+
+        while(true) {
+            if (corrente.successivo.successivo == null) {
+                //Punta al penultimo
+                int tmp = corrente.successivo.valore;
+                corrente.successivo = null;
+                return tmp;
+            }
+            corrente = corrente.successivo;
+        }
+        */
+
+        
+        Nodo corrente = this.testa;
+        while(corrente.successivo.successivo != null) {
+            corrente = corrente.successivo;
+        }
+        //Punta al penultimo
+        int tmp = corrente.successivo.valore;
+        corrente.successivo = null;
+        this.contaNodi--;
+        return tmp;
         
     }
+
+    /**
+     * Ritorna l'elemento in posizione data di una lista
+     * Ovviamente se la lista è vuota dovrebbe dare una eccezione
+     * Se la posizione è fuori range dovrebbe dare una eccezione
+     * @param posizione
+     * @return
+     */
+    public int get(int posizione) {
+        return 0;
+    }
+
+
+    
 }
